a35f46011bf4e43f8c72eebd550b265ce11e584d
Szymon Dzik <s24020@pjwstk.edu.pl>
2023-06-18 14:19:59 +0200

    initial commit

---
+++ estatesystem/src/main/java/com/estate/estatesystem/EstatesystemApplication.java
@@ -0,0 +1,13 @@
+package com.estate.estatesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatesystemApplication.class, args);
+	}
+
+}
---
+++ estatesystem/src/test/java/com/estate/estatesystem/EstatesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estate.estatesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
