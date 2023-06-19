f3b845c5a75862c23f0e2bf2ca0af52323c9b593
Szymon Dzik <s24020@pjwstk.edu.pl>
2023-06-19 11:05:29 +0200

    db setup

--- estatesystem/src/main/java/com/estate/estatesystem/EstateSystemApplication.java
+++ estatesystem/src/main/java/com/estate/estatesystem/EstateSystemApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EstateSystemApplication {
 
+	//spring.jpa.hibernate.ddl-auto=update
+	//spring.datasource.url=jdbc:mysql://localhost:3306/mas?useSSL=false
+	//spring.datasource.username=
+	//spring.datasource.password=
+	//spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+	//#spring.jpa.show-sql: true
+
 	public static void main(String[] args) {
 		SpringApplication.run(EstateSystemApplication.class, args);
 	}
